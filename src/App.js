b3dc62fcb3dff5fca48a4f6eb70530d209b98f63
beombu <sunil1369@bible.ac.kr>
2022-04-08 16:37:32 +0900

    fix : loginpage, registerpage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<MainPage />} />
                     <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
